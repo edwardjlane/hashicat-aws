783e3a0873260893051a574fa2f9d1a643e6af73
Edward Lane <edward.lane@computacenter.com>
2021-09-23 12:07:47 +0000

    Changed s3 synax again

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,9 +1,9 @@
 module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
 
-  bucket = "my-s3-bucket"
+  #bucket = "my-s3-bucket"
   bucket_prefix = "edwardlane"
   acl    = "private"
   version = "2.2.0"
-  
+
 }
