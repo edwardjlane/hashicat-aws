17b15199978f391721d54b81d3bceda02e1be8f8
Edward Lane <edward.lane@computacenter.com>
2021-09-23 11:23:51 +0000

    Added the first tag

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -9,7 +9,7 @@ cat << EOM > /var/www/html/index.html
   <div style="width:800px;margin: 0 auto">
 
   <!-- BEGIN -->
-  <center><img src="http://https://cdn-wp.thesportsrush.com/2021/09/e6aa3125-john-cena-thanks-wwe-hall-of-famer-for-teaching-him-valuable-lessons.jpg/${WIDTH}/${HEIGHT}"></img></center>
+  <center><img src="http://https://cdn-wp.thesportsrush.com/2021/09/e6aa3125-john-cena-thanks-wwe-hall-of-famer-for-teaching-him-valuable-lessons.jpg"></img></center>
   <center><h2>Meow World!</h2></center>
   Welcome to ${PREFIX}'s app. Replace this text with your own John Cena.
   <!-- END -->
